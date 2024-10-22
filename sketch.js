15ea09b9030ebfe12fe4c8271a49cdcb62183e59
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-22 13:24:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
 let firstBlock;
+let theStack
 
 function setup() {
   createCanvas(400, 400);
-  firstBlock = new Block(0,390,10,10,"#eb5e28")
+  firstBlock = new Block(0,390,100,10,"#eb5e28")
+  theStack = new Block(0,0,10,10,"#ccc5b9")
 }
 
 function draw() {
   background("#252422");
   firstBlock.display()
+  firstBlock.bounce()
+  theStack.display()
+  
+}
+
+function keyIsPressed(){
+  if(key === 's'){
+    
+  }
 }
