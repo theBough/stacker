e3e7f6dde8ced0b39b4daf487538639003d3f5f9
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-23 13:27:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,29 @@
-let firstBlock;
+let engine;
 let theStack
+let engineWidth;
+let engineHeight
 
 function setup() {
+  engineWidth = 200
+  engineHeight = 10
   createCanvas(400, 400);
-  firstBlock = new Block(0,390,100,10,"#eb5e28")
-  theStack = new Block(0,0,10,10,"#ccc5b9")
+  engine = new Block(0,height-engineHeight,engineWidth,engineHeight,"#eb5e28")
+  theStack = new Block(100,height-engineHeight,engineWidth,10,"#ccc5b9")
 }
 
 function draw() {
   background("#252422");
-  firstBlock.display()
-  firstBlock.bounce()
+  engine.display()
+  engine.bounce()
   theStack.display()  
 }
 
 function keyPressed(){
   if(key === 's'){
-    theStack.x = firstBlock.x
-    theStack.y = firstBlock.y
-    theStack.w = firstBlock.w
-    firstBlock.y -= 10
+    theStack.x = engine.x
+    theStack.y = engine.y
+    theStack.w = engine.w
+    engine.y -= 10
     theStack.h += 10
   }
 }
