115f515fc60858ba783107e8d5d11250b7d2cbf3
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-29 12:45:36 -0500

    Update winningAndLosing.js

--- winningAndLosing.js
+++ winningAndLosing.js
@@ -1,7 +1,11 @@
 function isWin(){
   if(engine.y<0){
-    text("Congratulations you just won an iPod mini",
-        350,
-        10)
+    push()
+    fill("#fffcf2");
+    textSize(25)
+    text("Congratulations\n you just won\n an iPod mini",
+        200,
+        200)
+    pop()
   }
 }
