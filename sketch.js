f32d205245a10dedfd78f098088899c7f077e231
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-31 13:22:59 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,15 +3,19 @@ let theStack = [];
 let engineWidth;
 let engineHeight
 let engineSpeed
+let gameOver;
+let outputMessage;
 
 function setup() {
+  gameOver = false;
   engineWidth = 100
   engineHeight = 30
   engineSpeed = 10
   createCanvas(400, 400);
-  engine = new Block(0,height-engineHeight*6,engineWidth,engineHeight,"#eb5e28",engineSpeed)
-  theStack.push(new Block(100,height-engineHeight*5,
-                          engineWidth,
+  engine = new Block(0,height-engineHeight*2,engineWidth,engineHeight,"#eb5e28",engineSpeed)
+  theStack.push(new Block(100,
+                          height-engineHeight,
+                          engineWidth*2,
                           engineHeight*5,
                           "#ccc5b9"))
 }
@@ -22,6 +26,15 @@ function draw() {
   engine.bounce()
   for(i=0; i< theStack.length; i++){
     theStack[i].display()  
+  }
+  if(gameOver){
+     push()
+    fill("#fffcf2");
+    textSize(25)
+    text(outputMessage,
+        200,
+        200)
+    pop()
   }
  
 }
