a70f3da84e82889723a2a5900f95bf50778128b5
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-31 13:11:14 -0500

    Update winningAndLosing.js

--- winningAndLosing.js
+++ winningAndLosing.js
@@ -1,11 +1,33 @@
 let outputMessage;
 function isWinOrIsLose(){
-  if(engine.y<0 || engine.w <1){
-    if(engine.y<0){
+  let lastStack = theStack.length - 1;
+  if(engine.y<0){
+    
       outputMessage = "Congratulations\n you just won\n an iPod mini"
-    }else{
+   
       outputMessage = "Wow you suck!"
-    }
+  
+    push()
+    fill("#fffcf2");
+    textSize(25)
+    text(outputMessage,
+        200,
+        200)
+    pop()
+    //stop the engine 
+    engine.speed = 0;
+  }
+  if(
+      (engine.x < theStack[lastStack].x &&
+      engine.x + engine.w < theStack[lastStack].x
+       ) ||
+      (
+      engine.x > theStack[lastStack].x + theStack[lastStack].w &&
+        engine.x + engine.w > theStack[lastStack].x + theStack[lastStack].w
+      )
+    ){
+     outputMessage = "Wow you suck!"
+  
     push()
     fill("#fffcf2");
     textSize(25)
