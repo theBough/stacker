69d4ad17627ff9026181a342b3eb53f46419041b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-31 12:51:16 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,34 @@
-function checkLeft() {
-  let lastStack = theStack.length - 1;
-  if (
-    engine.x < theStack[lastStack].x &&
-    engine.x + engine.w > theStack[lastStack].x
-  ) {
-    engine.w -= theStack[theStack.length - 1].x - engine.x;
-    engine.x = theStack[theStack.length - 1].x;
-    engine.y -= engineHeight;
-    //theStack.h += engineHeight;
-    //theStack.y -= engineHeight;
+let engine;
+let theStack = [];
+let engineWidth;
+let engineHeight
+let engineSpeed
 
-    theStack.push(
-      new Block(
-        theStack[lastStack].x,
-        engine.y + engineHeight,
-        Math.abs(theStack[lastStack].x - (engine.x + engine.w)),
-        engineHeight,
-        "#ccc5b9"
-      )
-    );
-  }
+function setup() {
+  engineWidth = 100
+  engineHeight = 30
+  engineSpeed = 10
+  createCanvas(400, 400);
+  engine = new Block(0,height-engineHeight*6,engineWidth,engineHeight,"#eb5e28",engineSpeed)
+  theStack.push(new Block(100,height-engineHeight*5,
+                          engineWidth,
+                          engineHeight*5,
+                          "#ccc5b9"))
 }
-function checkRight() {
-  //check to see if the engine is on the stack but hanging over rightside
-  let lastStack = theStack.length - 1;
-  if (
-    engine.x > theStack[lastStack].x &&
-    engine.x < theStack[lastStack].x + theStack[lastStack].w &&
-    engine.x + engine.w > theStack[lastStack].x + theStack[lastStack].w
-  ) {
-    engine.w =
-      engine.w -
-      (engine.x + engine.w - (theStack[lastStack].x + theStack[lastStack].w));
-    engine.y -= engineHeight;
-    theStack.push(
-      new Block(
-        engine.x,
-        engine.y + engineHeight,
-        engine.w,
-        engineHeight,
-        "#ccc5b9"
-      )
-    );
+
+function draw() {
+  background("#252422");
+  engine.display()
+  engine.bounce()
+  for(i=0; i< theStack.length; i++){
+    theStack[i].display()  
+  }
+ isWinOrIsLose();
+}
+
+function keyPressed(){
+  if(key === 's'){
+    checkLeft();
+    checkRight();
   }
 }
