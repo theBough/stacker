54bdb09910353bc6023822c408d375b401f2b94e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-31 12:55:37 -0500

    Update checkLeftAndRight.js

--- checkLeftAndRight.js
+++ checkLeftAndRight.js
@@ -1,32 +1,46 @@
-function checkLeft(){
-    engine.w -= theStack[theStack.length -1].x - engine.x
-    engine.x = theStack[theStack.length -1].x
+function checkLeft() {
+  let lastStack = theStack.length - 1;
+  if (
+    engine.x < theStack[lastStack].x &&
+    engine.x + engine.w > theStack[lastStack].x
+  ) {
+    engine.w -= theStack[theStack.length - 1].x - engine.x;
+    engine.x = theStack[theStack.length - 1].x;
     engine.y -= engineHeight;
     //theStack.h += engineHeight;
     //theStack.y -= engineHeight;
-    
-    theStack.push (new Block(theStack[0].x,
-                  engine.y+engineHeight,
-                  Math.abs(theStack[theStack.length -1].x - (engine.x+engine.w)),
-                  engineHeight,
-                  "#ccc5b9"
-                 ))
-    
+
+    theStack.push(
+      new Block(
+        theStack[lastStack].x,
+        engine.y + engineHeight,
+        Math.abs(theStack[lastStack].x - (engine.x + engine.w)),
+        engineHeight,
+        "#ccc5b9"
+      )
+    );
+  }
 }
-function checkRight(){
+function checkRight() {
   //check to see if the engine is on the stack but hanging over rightside
-  let lastStack = theStack.length -1;
-  if(engine.x > theStack[lastStack].x &&
-     engine.x < theStack[lastStack].x + theStack[lastStack].w &&
-    engine.x + engine.w > theStack[lastStack].x + theStack[lastStack].w)
-  {
-   engine.w = engine.w-((engine.x+engine.w) -(theStack[lastStack].x + theStack[lastStack].w) )
+  let lastStack = theStack.length - 1;
+  if (
+    engine.x > theStack[lastStack].x &&
+    engine.x < theStack[lastStack].x + theStack[lastStack].w &&
+    engine.x + engine.w > theStack[lastStack].x + theStack[lastStack].w
+  ) {
+    engine.w =
+      engine.w -
+      (engine.x + engine.w - (theStack[lastStack].x + theStack[lastStack].w));
     engine.y -= engineHeight;
-    theStack.push (new Block(engine.x,
-                  engine.y+engineHeight,
-                  engine.w,
-                  engineHeight,
-                  "#ccc5b9"
-                 ))
+    theStack.push(
+      new Block(
+        engine.x,
+        engine.y + engineHeight,
+        engine.w,
+        engineHeight,
+        "#ccc5b9"
+      )
+    );
   }
 }
